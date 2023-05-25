f4c964919c29767b782e08f126129ba50bd494a5
“JeffersonMedines” <jeffs.medines@gmail.com>
2023-05-25 20:07:55 -0300

    update run

--- run_model.sh
+++ run_model.sh
@@ -4,4 +4,4 @@ data=$(date '+%Y-%m-%dT%H:%M:%S')
 # path
 path='/home/jeffsmedines/repos/loyalty_program'
 
-papermill $path/src/models/v09_loyalty_program_model_deployment_aws.ipynb $path/reports/logs/v09_loyalty_program_model_deployment_aws_$data.ipynb
+papermill /src/models/v09_loyalty_program_model_deployment_aws.ipynb /reports/logs/v09_loyalty_program_model_deployment_aws_$data.ipynb
